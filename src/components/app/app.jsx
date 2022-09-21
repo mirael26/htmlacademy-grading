cfbe6188b0fe95d0513924b43109658a4ce5c143
Elmira Ilyasova <capricorn_26@mail.ru>
2022-09-21 23:40:45 +0700

    Add starter pack

---
+++ src/components/app/app.jsx
@@ -0,0 +1,33 @@
+import { ThemeProvider } from 'styled-components';
+import {
+  Switch,
+  Route,
+  BrowserRouter as Router,
+} from 'components/common/common';
+import DetailedQuest from 'components/detailed-quest/detailed-quest';
+import Contacts from 'components/contacts/contacts';
+import Home from 'components/home/home';
+import NotFound from 'components/not-found/not-found';
+import { appTheme } from './common';
+import * as S from './app.styled';
+
+const App = () => (
+  <ThemeProvider theme={appTheme}>
+    <S.GlobalStyle />
+    <Router>
+      <Switch>
+        <Route exact path="/quest">
+          <DetailedQuest />
+        </Route>
+        <Route exact path="/contacts">
+          <Contacts />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  </ThemeProvider>
+);
+
+export default App;
