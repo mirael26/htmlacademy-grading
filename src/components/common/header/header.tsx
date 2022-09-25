5853f70ee455b94e1f22e29f94e5723b0a9f7cbd
Elmira Ilyasova <capricorn_26@mail.ru>
2022-09-25 16:25:09 +0700

    Add changing style for Header nav menu

--- src/components/common/header/header.tsx
+++ src/components/common/header/header.tsx
@@ -1,42 +1,47 @@
 import logo from 'assets/img/logo.svg';
 import { AppUrl } from 'consts';
+import { useLocation } from 'react-router-dom';
 import * as S from './header.styled';
 
-const Header = () => (
-  <S.StyledHeader>
-    <S.HeaderWrapper>
-      <S.Logo>
-        <S.Image src={logo} alt="Логотип Escape Room" width="134" height="50" />
-      </S.Logo>
+const Header = () => {
+  const {pathname} = useLocation();
 
-      <S.Navigation>
-        <S.Links>
-          <S.LinkItem>
-            <S.Link $isActiveLink to={AppUrl.Home}>
-              Квесты
-            </S.Link>
-          </S.LinkItem>
+  return (
+    <S.StyledHeader>
+      <S.HeaderWrapper>
+        <S.Logo>
+          <S.Image src={logo} alt="Логотип Escape Room" width="134" height="50" />
+        </S.Logo>
 
-          <S.LinkItem>
-            <S.Link to="#">Новичкам</S.Link>
-          </S.LinkItem>
+        <S.Navigation>
+          <S.Links>
+            <S.LinkItem>
+              <S.Link $isActiveLink={pathname === AppUrl.Home} to={AppUrl.Home}>
+                Квесты
+              </S.Link>
+            </S.LinkItem>
 
-          <S.LinkItem>
-            <S.Link to="#">Отзывы</S.Link>
-          </S.LinkItem>
+            <S.LinkItem>
+              <S.Link to="#">Новичкам</S.Link>
+            </S.LinkItem>
 
-          <S.LinkItem>
-            <S.Link to="#">Акции</S.Link>
-          </S.LinkItem>
+            <S.LinkItem>
+              <S.Link to="#">Отзывы</S.Link>
+            </S.LinkItem>
 
-          <S.LinkItem>
-            <S.Link to={AppUrl.Contacts}>Контакты</S.Link>
-          </S.LinkItem>
-        </S.Links>
-      </S.Navigation>
-      <S.Phone href="tel:88003335599">8 (800) 333-55-99</S.Phone>
-    </S.HeaderWrapper>
-  </S.StyledHeader>
-);
+            <S.LinkItem>
+              <S.Link to="#">Акции</S.Link>
+            </S.LinkItem>
+
+            <S.LinkItem>
+              <S.Link $isActiveLink={pathname === AppUrl.Contacts} to={AppUrl.Contacts}>Контакты</S.Link>
+            </S.LinkItem>
+          </S.Links>
+        </S.Navigation>
+        <S.Phone href="tel:88003335599">8 (800) 333-55-99</S.Phone>
+      </S.HeaderWrapper>
+    </S.StyledHeader>
+  )
+};
 
 export default Header;
